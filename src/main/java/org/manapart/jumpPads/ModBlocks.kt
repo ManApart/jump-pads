c9d166644c80c46f066d0aa56090a8f8b11fc0cf
ManApart <r@austinkucera.com>
2022-06-23 12:59:44 -0400

    use proper registry fetching

--- src/main/java/org/manapart/jumpPads/ModBlocks.kt
+++ src/main/java/org/manapart/jumpPads/ModBlocks.kt
@@ -5,14 +5,10 @@ import net.minecraftforge.registries.ForgeRegistries
 import thedarkcolour.kotlinforforge.forge.registerObject
 
 object ModBlocks {
-    val jumpPad = JumpPad(1.5f)
-    val jumpPad2 = JumpPad(3f)
-    val jumpPad3 = JumpPad(4f)
-
     val REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID)
 
-    val JUMP_PAD by REGISTRY.registerObject("jumppad") { jumpPad }
-    val JUMP_PAD_2 by REGISTRY.registerObject("jumppad_2") { jumpPad2 }
-    val JUMP_PAD_3 by REGISTRY.registerObject("jumppad_3") { jumpPad3 }
+    val JUMP_PAD by REGISTRY.registerObject("jumppad") { JumpPad(1.5f) }
+    val JUMP_PAD_2 by REGISTRY.registerObject("jumppad_2") { JumpPad(3f) }
+    val JUMP_PAD_3 by REGISTRY.registerObject("jumppad_3") { JumpPad(4f) }
 
 }
--- src/main/java/org/manapart/jumpPads/ModItems.kt
+++ src/main/java/org/manapart/jumpPads/ModItems.kt
@@ -8,7 +8,7 @@ import thedarkcolour.kotlinforforge.forge.registerObject
 object ModItems {
     val REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, MODID)
 
-    val JUMP_PAD_ITEM by REGISTRY.registerObject("jumppaditem") { JumpPadItem(ModBlocks.jumpPad) }
-    val JUMP_PAD_ITEM_2 by REGISTRY.registerObject("jumppaditem_2") { JumpPadItem(ModBlocks.jumpPad2) }
-    val JUMP_PAD_ITEM_3 by REGISTRY.registerObject("jumppaditem_3") { JumpPadItem(ModBlocks.jumpPad3) }
+    val JUMP_PAD_ITEM by REGISTRY.registerObject("jumppaditem") { JumpPadItem(ModBlocks.JUMP_PAD) }
+    val JUMP_PAD_ITEM_2 by REGISTRY.registerObject("jumppaditem_2") { JumpPadItem(ModBlocks.JUMP_PAD_2) }
+    val JUMP_PAD_ITEM_3 by REGISTRY.registerObject("jumppaditem_3") { JumpPadItem(ModBlocks.JUMP_PAD_3) }
 }
